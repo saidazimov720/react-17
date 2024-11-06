27bd55818e69f04c6112f4b643bd15847a3635c3
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-06 10:46:23 +0500

    return if else

--- src/App.tsx
+++ src/App.tsx
@@ -5,5 +5,14 @@ import VerificationPage from './components/Verification';
 const App = () => {
   const [submittedData, setSubmittedData] = useState<Record<string, any> | null>(null);
 
-  
+  return(
+    <>
+    {submittedData? (
+      <VerificationPage data={submittedData} edit={() => setSubmittedData(null)}/>
+    ):(
+      <AdmissionForm onSubmit={(data) => setSubmittedData(data)}/>
+    )
+    }
+    </>
+  )
 }
