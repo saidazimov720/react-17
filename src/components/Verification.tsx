7e18e2566a2cf535ac62679acc831954ea2625ac
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-06 10:22:24 +0500

    verification Page function

--- src/components/Verification.tsx
+++ src/components/Verification.tsx
@@ -5,3 +5,11 @@ interface VerificationProps{
     data: Record<string, any>;
     onEdit: () => void;
 }
+
+const VerificationPage: React.FC<VerificationProps> = ({data, edit}) =>{
+    return(
+        <Box sx={{maxWidth: 700, margin: '0 auto' , padding: '2rem'}}>
+            <Typography variant="h4" ></Typography>
+        </Box>
+    )
+}
