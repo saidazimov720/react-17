7624f8aba6d00bb9d733c7e780e30204fb166b2d
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-06 10:17:42 +0500

    import the material ui and verification Props

--- src/components/Verification.tsx
+++ src/components/Verification.tsx
@@ -1,1 +1,7 @@
 import React from "react";
+import { Box, Typography, Grid, Button } from "@mui/material";
+
+interface VerificationProps{
+    data: Record<string, any>;
+    onEdit: () => void;
+}
